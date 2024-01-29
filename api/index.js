02fb8e825dc6d525e47c847f0f5ef6d657ebc703
Vamsi Krishna Reddy Munnangi <vamsikrishna1827@gmail.com>
2024-01-29 16:12:14 -0500

    Connect to Mongo Database

--- api/index.js
+++ api/index.js
@@ -1,12 +1,19 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 
+dotenv.config()
+
+mongoose.connect(process.env.MONGO)
+.then(()=>{console.log("MongoDb is Connected")})
+.catch((err)=>{console.log(err)})
 const app = express()
 
 app.listen(3000, ()=>{
     console.log("Server is running on port 3000!")
 })
 
-app.get('/hello',(req,res)=>{
-    console.log("Hello")
-    res.status(200).send("Hello")
-})
+// app.get('/hello',(req,res)=>{
+//     console.log("Hello")
+//     res.status(200).send("Hello")
+// })
